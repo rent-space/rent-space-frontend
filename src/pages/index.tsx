5d6f5611576abcc2cb1ee8e28bef1e042a07eefa
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-17 23:47:05 -0300

    adicionando componente de input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { NameInput } from "@/components/NameInput";
+import { BiSearch } from "react-icons/bi";
+import { AiFillMail } from 'react-icons/ai'
+
 export default function Home() {
-  return <h1>RentSpace</h1>;
+  return (
+    <>
+    <h1>RentSpace</h1>
+    <NameInput title="Email" placeholder="exemplo@gmail.com" />
+    <NameInput title="Email" placeholder="exemplo@gmail.com" icon={BiSearch}/>
+    <NameInput title="Email" placeholder="exemplo@gmail.com" icon={AiFillMail}/>
+    </>
+  )
 }
