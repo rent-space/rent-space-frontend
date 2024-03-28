31007899828f6d31809aa7a3d20ed72c0bf30844
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-27 21:05:47 -0300

    adicionando term-conditions

--- src/components/TermConditions/index.tsx
+++ src/components/TermConditions/index.tsx
@@ -4,9 +4,12 @@ import styles from './styles.module.css';
 export function TermCondition() {
     return (
         <div className={styles.container}>
-            <span>Li e aceito os <Link href="" className={styles.link}> termos e condições</Link></span>
-            <input type="checkbox" />
+            <label className={styles.checkboxLabel}>
+
+                <span>Li e aceito os <Link href="" className={styles.link}>termos e condições</Link></span>
+                <input type="checkbox" className={styles.checkboxInput} name="termos-e-condicoes"/>
+                
+            </label>
         </div>
-        
     )
 }
