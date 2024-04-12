30a44945de71ca391d094d1d86669cb7e2ab7d1a
Kilian Melcher <kilian.melcher@vtex.com>
2024-04-12 00:23:34 -0300

    Removeu unnecessary session and router from user avtar

--- src/components/UserAvatar/index.tsx
+++ src/components/UserAvatar/index.tsx
@@ -1,13 +1,9 @@
-import { useRouter } from "next/router";
 import { IconAccountCircle } from "../Icons/IconAccountCircle";
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export function UserAvatar() {
-  const router = useRouter();
-  // const {session: } = useSession();
-
-  const logout = async () => {
-    await signOut({ callbackUrl: "/" });
+  const logout = () => {
+    signOut({ callbackUrl: "/" });
   };
 
   return (
