60d2ab3263cc6e2046ca6634a136604e42a1b9ef
Kilian Melcher <kilian.melcher@vtex.com>
2024-04-30 20:54:47 -0300

    Fix home spaces css

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,11 +7,13 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import HomeImage from "@/assets/home_image.svg";
 import { useRouter } from "next/router";
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 import { CardDescription } from "@/components/CardDescription";
 import { useEffect, useState } from "react";
 import { AllSpaces } from "@/utils/types";
 import { getSpaces } from "@/services/api/space";
+import { Page } from "@/components/Page";
+import { Text } from "@/components/Text";
 
 export default function Home() {
   const router = useRouter();
@@ -50,47 +52,58 @@ export default function Home() {
         <NavBar />
         <UserAvatar />
       </Header>
-      <div className={styles.container}>
-        <Image
-          src={HomeImage}
-          alt="RentSpace Logo"
-          style={{filter: "brightness(0.5)"}}
-        />  
-        <div className={styles.overlayText}>
-          <div className={styles.titleContainer}>
-            <span className={styles.eventPhrase}>Vamos organizar esse</span>
-            <span className={styles.eventPhraseBold}>EVENTO ?</span>
-          </div>
-          <div className={styles.subtitleContainer}>
-            <span className={styles.subtitle}>Seja bem-vindo!  Estamos aqui para oferecer
-            as principais necessidades para que você tenha o evento perfeito sem qualquer preocupação!</span>
+      <Page>
+        <div className={styles.container}>
+          <Image
+            src={HomeImage}
+            alt="RentSpace Logo"
+            style={{ filter: "brightness(0.5)" }}
+          />
+          <div className={styles.overlayText}>
+            <div className={styles.titleContainer}>
+              <span className={styles.eventPhrase}>Vamos organizar esse</span>
+              <span className={styles.eventPhraseBold}>EVENTO ?</span>
+            </div>
+            <div className={styles.subtitleContainer}>
+              <span className={styles.subtitle}>
+                Seja bem-vindo! Estamos aqui para oferecer as principais
+                necessidades para que você tenha o evento perfeito sem qualquer
+                preocupação!
+              </span>
+            </div>
           </div>
         </div>
 
-        <div className={styles.textContainer}>
-          <span className={styles.spaceText}>
-            Principais Espaços
-          </span>
-          <button className={styles.seeAll} onClick={navigateToListSpace}>
-            Ver todos
-          </button>
+        <div className={styles.spacesContainter}>
+          <div className={styles.spaces}>
+            <div className={styles.textContainer}>
+              <Text size="title1" weight="semibold">
+                Principais Espaços
+              </Text>
+              <button className={styles.seeAll} onClick={navigateToListSpace}>
+                Ver todos
+              </button>
+            </div>
+
+            <div className={styles.cardContainer}>
+              {cards.map((card, _) => {
+                return (
+                  <div className={styles.cards} key={card.id}>
+                    <CardDescription
+                      title={card.title}
+                      maxCapacity={card.maximumCapacity}
+                      image={card.media?.length ? card.media[0] : ""}
+                      pricePerHour={card.pricePerHour}
+                      onClick={() => navigateToDetailsSpace(card.id)}
+                      description={card.description}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <div className={styles.cardContainer}>
-          {cards.map((card,_) => {
-            return (
-              <div className={styles.cards} key={card.id}>
-                <CardDescription
-                  title={card.title}
-                  maxCapacity={card.maximumCapacity}
-                  image={card.media?.length ? card.media[0] : ""}
-                  pricePerHour={card.pricePerHour}
-                  onClick={() => navigateToDetailsSpace(card.id)}
-                  description={card.description}
-                />
-              </div>
-          )})}
-        </div>
-      </div>
+      </Page>
     </>
   );
 }
