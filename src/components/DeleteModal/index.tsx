16ee825ded72b51c9463ecb4385a725cca7e1d70
Kilian Melcher <kilian.melcher@vtex.com>
2024-05-05 10:52:07 -0300

    Add loading to delete space

--- src/components/DeleteModal/index.tsx
+++ src/components/DeleteModal/index.tsx
@@ -2,49 +2,70 @@ import { FiTrash } from "react-icons/fi";
 import { Button } from "../Button";
 import styles from "./styles.module.css";
 import { FaRegTimesCircle } from "react-icons/fa";
-import {deleteSpace} from '../../../src/services/api/space'
+import { deleteSpace } from "../../../src/services/api/space";
 import { useRouter } from "next/router";
+import { useState } from "react";
 interface Props {
   modal: boolean;
   close: () => void;
   deleteSpaceId: number;
 }
 
-
 export default function DeleteModal(props: Props) {
   const { close, modal, deleteSpaceId } = props;
-  const router = useRouter();
 
- const handleDeleteSpace = (id:number) => {
-    deleteSpace(id);
-    router.push("/spaces");
-  }
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleDeleteSpace = (id: number) => {
+    setLoading(true);
+    deleteSpace(id)
+      .then((res) => {
+        setLoading(false);
+        res && router.push("/spaces");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  };
 
   if (!modal) return null;
 
   return (
     <>
-    <div className={styles.background}>
-      <div className={styles.modal}>
-        <FaRegTimesCircle className={styles.close} size={24} onClick={close} style={{color:'#bdbdbd'}}/>
-        <div className={styles.textContainer}>
-          <span className={styles.title}>
-            Deseja excluir o espaço?
-          </span>
-          <p className={styles.subtitle}>
-            Tem certeza que deseja excluir o espaço? Essa ação é irrevesível.
-          </p>
-        </div>
-        <div className={styles.buttonContainer}>
-          <Button size="small" variant="secondary" onClick={close} > Cancelar </Button>
-          <Button size="small" variant="primary" className={styles.removeButton} onClick={() => {handleDeleteSpace(deleteSpaceId)}}>
-            Excluir espaço
-            <FiTrash style={{paddingLeft:'0.7rem'}}/>
-          </Button>
-
+      <div className={styles.background}>
+        <div className={styles.modal}>
+          <FaRegTimesCircle
+            className={styles.close}
+            size={24}
+            onClick={close}
+            style={{ color: "#bdbdbd" }}
+          />
+          <div className={styles.textContainer}>
+            <span className={styles.title}>Deseja excluir o espaço?</span>
+            <p className={styles.subtitle}>
+              Tem certeza que deseja excluir o espaço? Essa ação é irrevesível.
+            </p>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button size="small" variant="secondary" onClick={close}>
+              Cancelar
+            </Button>
+            <Button
+              size="small"
+              variant="primary"
+              className={styles.removeButton}
+              onClick={() => {
+                handleDeleteSpace(deleteSpaceId);
+              }}
+            >
+              {loading ? "Excluindo..." : "Excluir espaço"}
+              <FiTrash style={{ paddingLeft: "0.7rem" }} />
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/services/api/utils.ts
+++ src/services/api/utils.ts
@@ -23,7 +23,6 @@ export async function fetchApi(url: string, options?: FetchOptions) {
   if (options?.isUrlEncoded) {
     contentType = "application/x-www-form-urlencoded";
   }
-  console.log("contentType", contentType);
   try {
     const response = await api({
       url,
@@ -32,11 +31,10 @@ export async function fetchApi(url: string, options?: FetchOptions) {
       params: options?.params,
       headers: {
         "Content-Type": contentType,
+        crossDomain: true,
       },
     });
-    console.log("response", response);
 
-    console.log(response);
     return { data: response.data, error: null };
   } catch (error: any) {
     console.error(error);
