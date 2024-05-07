681bc25a1d31d78f083c35e023501672247dc4c7
Kilian Melcher <kilian.melcher@vtex.com>
2024-05-07 20:34:19 -0300

    Remove cross domain

--- src/services/api/utils.ts
+++ src/services/api/utils.ts
@@ -31,7 +31,6 @@ export async function fetchApi(url: string, options?: FetchOptions) {
       params: options?.params,
       headers: {
         "Content-Type": contentType,
-        crossDomain: true,
       },
     });
 
