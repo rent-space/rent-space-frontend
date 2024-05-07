7b53f622ab0369c177f180f80c145f1a09a577b9
Kilian Melcher <kilian.melcher@vtex.com>
2024-05-07 20:42:29 -0300

    Fix service owner user type

--- src/pages/requested-solicitations/index.tsx
+++ src/pages/requested-solicitations/index.tsx
@@ -4,87 +4,101 @@ import { UserAvatar } from "@/components/UserAvatar";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
-import styles from './styles.module.css';
-import { getPlaceReservationById, getPlaceReservations } from "@/services/api/reservations";
+import styles from "./styles.module.css";
+import {
+  getPlaceReservationById,
+  getPlaceReservations,
+} from "@/services/api/reservations";
 import { PlaceReservation } from "@/utils/types";
 import PageCard from "./_card";
 import Loading from "@/components/Loading";
 
 interface PageDataProps {
-  title: string,
-  firstSection: string,
-  secSection: string
+  title: string;
+  firstSection: string;
+  secSection: string;
 }
 
 export default function RequestedSolicitations() {
   const { data } = useSession();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [shouldRespond, setShouldRespond] = useState<boolean>(false);
-  const [allReservations, setAllReservations] = useState<PlaceReservation[] | null>(null);
-  const [pedingReservation, setPendingReservation] = useState<PlaceReservation[] | null>(null);
-  const [answeredReservation, setAnsweredReservation] = useState<PlaceReservation[] | null>(null);
+  const [allReservations, setAllReservations] = useState<
+    PlaceReservation[] | null
+  >(null);
+  const [pedingReservation, setPendingReservation] = useState<
+    PlaceReservation[] | null
+  >(null);
+  const [answeredReservation, setAnsweredReservation] = useState<
+    PlaceReservation[] | null
+  >(null);
   const [pageData, setPageData] = useState<PageDataProps>({
     title: "Suas reservas",
     firstSection: "Solicitações pendentes",
-    secSection: "Solicitações respondidas"
+    secSection: "Solicitações respondidas",
   });
 
   useEffect(() => {
     const getReservation = async () => {
-      getPlaceReservations().then(reservations => {
-        setAllReservations(reservations)
+      getPlaceReservations().then((reservations) => {
+        setAllReservations(reservations);
       });
-    }
+    };
 
     getReservation();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    if (data?.user?.userType === 'PLACE_OWNER' || data?.user?.userType === 'SERVICE_OFFER') {
+    if (
+      data?.user?.userType === "PLACE_OWNER" ||
+      data?.user?.userType === "SERVICE_OWNER"
+    ) {
       const pageInfos = {
         title: "Solicitações recebidas",
         firstSection: "Reservas requisitadas",
-        secSection: "Reservas respondidas"
-      }
+        secSection: "Reservas respondidas",
+      };
       setShouldRespond(true);
 
       setPageData({
         ...pageData,
-        ...pageInfos
-      })
+        ...pageInfos,
+      });
     }
-  }, [data?.user])
+  }, [data?.user]);
 
   useEffect(() => {
     if (data?.user.email && isLoading) {
-      getReservationsForUser(data?.user.email)
+      getReservationsForUser(data?.user.email);
     }
-  }, [data?.user, allReservations])
+  }, [data?.user, allReservations]);
 
   const getReservationsForUser = (userEmail: string) => {
     if (allReservations != null) {
-      let pendingList: PlaceReservation[]  = [];
+      let pendingList: PlaceReservation[] = [];
       let answeredList: PlaceReservation[] = [];
-      console.log(allReservations)
-    
-      allReservations.forEach(reserv => {
-        if (reserv.status == "PENDING" && 
-          (reserv.product.owner.email == userEmail || 
-          reserv.eventOwner.email == userEmail)
+      console.log(allReservations);
+
+      allReservations.forEach((reserv) => {
+        if (
+          reserv.status == "PENDING" &&
+          (reserv.product.owner.email == userEmail ||
+            reserv.eventOwner.email == userEmail)
         ) {
           pendingList.push(reserv);
-        } else if ((reserv.product.owner.email == userEmail || 
-          reserv.eventOwner.email == userEmail)
+        } else if (
+          reserv.product.owner.email == userEmail ||
+          reserv.eventOwner.email == userEmail
         ) {
           answeredList.push(reserv);
         }
-      })
-    
+      });
+
       setIsLoading(false);
       setPendingReservation(pendingList);
       setAnsweredReservation(answeredList);
     }
-  }
+  };
 
   return (
     <>
@@ -100,15 +114,19 @@ export default function RequestedSolicitations() {
           <span className={styles.sectionTitle}>{pageData.firstSection}</span>
 
           <div className={styles.cards}>
-            {isLoading ?
+            {isLoading ? (
               <Loading loadingLabel="Carregando suas reservas..." />
-              : 
-              pedingReservation !== null && pedingReservation.length > 0 ?
-                pedingReservation.map((reser, index) =>
-                  <PageCard placeReservation={reser} key={index} shouldClick={shouldRespond} />
-                ) :
+            ) : pedingReservation !== null && pedingReservation.length > 0 ? (
+              pedingReservation.map((reser, index) => (
+                <PageCard
+                  placeReservation={reser}
+                  key={index}
+                  shouldClick={shouldRespond}
+                />
+              ))
+            ) : (
               <span>Não há nenhuma solicitação</span>
-            }
+            )}
           </div>
         </div>
 
@@ -116,18 +134,23 @@ export default function RequestedSolicitations() {
           <span className={styles.sectionTitle}>{pageData.secSection}</span>
 
           <div className={styles.cards}>
-            {isLoading ?
+            {isLoading ? (
               <Loading loadingLabel="Carregando suas reservas..." />
-              :
-              answeredReservation !== null && answeredReservation.length > 0 ?
-                answeredReservation.map((reser, index) =>
-                  <PageCard placeReservation={reser} key={index} shouldClick={false} />
-                ) :
+            ) : answeredReservation !== null &&
+              answeredReservation.length > 0 ? (
+              answeredReservation.map((reser, index) => (
+                <PageCard
+                  placeReservation={reser}
+                  key={index}
+                  shouldClick={false}
+                />
+              ))
+            ) : (
               <span>Não há nenhuma solicitação</span>
-            }
+            )}
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
